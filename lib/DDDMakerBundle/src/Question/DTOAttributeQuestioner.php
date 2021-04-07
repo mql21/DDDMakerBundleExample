55443f63b16c8afb74bba8b3702775b6eab4e013
Miquel Perez <mql21.12@gmail.com>
2021-04-07 16:29:29 +0200

    Allow and suggest primitive types when asking for class attributes

--- lib/DDDMakerBundle/src/Question/DTOAttributeQuestioner.php
+++ lib/DDDMakerBundle/src/Question/DTOAttributeQuestioner.php
@@ -1,8 +1,10 @@
 <?php
+
 namespace Mql21\DDDMakerBundle\Question;
 
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\ChoiceQuestion;
 use Symfony\Component\Console\Question\ConfirmationQuestion;
 use Symfony\Component\Console\Question\Question;
 
@@ -12,21 +14,25 @@ class DTOAttributeQuestioner
     {
         $attributes = [];
         $continueAskingAttributes = true;
-        while($continueAskingAttributes) {
+        while ($continueAskingAttributes) {
             $attributeNameQuestion = new Question("<info> What should the attribute be called?</info>\n > ");
             $attributeName = $questionHelper->ask($input, $output, $attributeNameQuestion);
-    
-            $attributeTypeQuestion = new Question("<info> What type should the attribute be?</info>\n > ");
+            $availableTypes = ["string", "int", "float", "bool", "array"]; // TODO: abstract this
+            $attributeTypeQuestion = new ChoiceQuestion(
+                "<info> What type should the attribute be?</info>\n > ",
+                $availableTypes
+            );
+            $attributeTypeQuestion->setAutocompleterValues($availableTypes);
             $attributeType = $questionHelper->ask($input, $output, $attributeTypeQuestion);
             
             $attributes[$attributeName] = $attributeType;
-                
+            
             $createQueryHandlerQuestion = new ConfirmationQuestion(
                 "<info> Do you wish to add another attribute (y/n)?</info>\n > ",
                 false,
                 '/^(y|s)/i'
             );
-    
+            
             $continueAskingAttributes = $questionHelper->ask($input, $output, $createQueryHandlerQuestion);
         }
         
