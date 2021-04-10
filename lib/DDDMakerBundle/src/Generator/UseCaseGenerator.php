65b3d64985da7db57112d0f35473abf8e442065e
Miquel Perez <mql21.12@gmail.com>
2021-04-10 16:48:36 +0200

    add template for use cases

--- lib/DDDMakerBundle/src/Generator/UseCaseGenerator.php
+++ lib/DDDMakerBundle/src/Generator/UseCaseGenerator.php
@@ -5,6 +5,7 @@ namespace Mql21\DDDMakerBundle\Generator;
 use Mql21\DDDMakerBundle\Exception\ElementAlreadyExistsException;
 use Mql21\DDDMakerBundle\Factories\PathFactory;
 use Mql21\DDDMakerBundle\Generator\Contract\DDDElementGenerator;
+use Mql21\DDDMakerBundle\Templates\DTOTemplateData;
 
 class UseCaseGenerator implements DDDElementGenerator
 {
@@ -18,7 +19,18 @@ class UseCaseGenerator implements DDDElementGenerator
             throw new ElementAlreadyExistsException("Use case {$useCaseName} already exists in module \"{$moduleName}\" of bounded context \"{$boundedContextName}\".");
         }
     
-        file_put_contents($useCaseFullPath, "<?php \n\nnamespace Test\Module;\n\nclass {$useCaseName}\n{\n}\n");
+        file_put_contents($useCaseFullPath, $this->renderResponseTemplate($useCaseName));
     }
     
+    // TODO extract and inject as collaborator
+    protected function renderResponseTemplate(string $useCaseName): string
+    {
+        $template = file_get_contents(
+            "lib/DDDMakerBundle/src/Templates/use_case.php.template"
+        ); //TODO: get via config (DI)
+        $classContent = str_replace("{{t_namespace}}", "Mql21\DDDMakerBundle\Generator", $template);
+        $classContent = str_replace("{{t_class_name}}", $useCaseName, $classContent);
+        
+        return $classContent;
+    }
 }
